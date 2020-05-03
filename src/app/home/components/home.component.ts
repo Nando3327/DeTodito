a8ac4cc03d122e02b9d9f2e709fcc75f00378406
Fernando Castillo <fernando.castillo@easysoft.com.ec>
2020-05-03 17:38:44 -0500

    routers

--- src/app/home/components/home.component.ts
+++ src/app/home/components/home.component.ts
@@ -1,21 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   loadComponent = false;
-  constructor(
-  ) {
+  mobileQuery: MediaQueryList;
+  fillerNav: Array<any>;
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
   ngOnInit(): void {
+    this.fillerNav = [
+      {
+        url: '/home/categories',
+        text: 'Categories'
+      },
+      {
+        url: '/',
+        text: 'LogOut'
+      }
+    ];
   }
 
-  chargeComponent(): void{
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  chargeComponent(snav): void {
     this.loadComponent = true;
+    snav.toggle();
   }
 }
